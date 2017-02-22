c09dc3623d471781df921e59b79e20b2ae898034
Jeevitha <jeevithaaiyappa09@gmail.com>
2017-02-22 15:35:38 +0530

    Changes related to Backbutton issue

--- www/js/directives.js
+++ www/js/directives.js
@@ -262,4 +262,18 @@
     }
   }
 })
+  .directive('backButton', function(){
+    return {
+      restrict: 'A',
+
+      link: function(scope, element, attrs) {
+        element.bind('click', goBack);
+
+        function goBack() {
+          history.back();
+          scope.$apply();
+        }
+      }
+    }
+})
 })();
